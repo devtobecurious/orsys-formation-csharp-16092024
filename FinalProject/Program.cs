bd3649f1940d4823674d2db46ca4c67ce38d4ff7
Evan BOISSONNOT <evan@boissonnot.fr>
2024-09-16 17:14:12 +0200

    feat: premiere journée

---
+++ FinalProject/Program.cs
@@ -0,0 +1,91 @@
+﻿System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+
+Console.WriteLine("A Sam journey");
+
+#region Exécution
+DisplayMenu();
+int choice = GetChoiceFromUser();
+ExecuteActionByChoice(choice);
+#endregion
+
+
+#region Code interne
+int GetChoiceFromUser()
+{
+    Console.WriteLine("Saisis ton choix");
+    var choiceString = Console.ReadLine();
+
+    int menuIndex = -1;
+    if (!int.TryParse(choiceString, out menuIndex))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Oops mauvaise manip");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    return menuIndex;
+}
+
+void DisplayMenu()
+{
+    string displayFormat = "{0}. {1}";
+
+    Console.WriteLine(string.Format(displayFormat, 1.ToString(), "Nouvelle partie", "toto"));
+    Console.WriteLine(string.Format(displayFormat, 2, "Charger une partie"));
+
+    //Console.WriteLine(string.Format(displayFormat, 3, "Credits"));
+    var credits = "Crédits";
+    Console.WriteLine($"{GetLastIndex()}. {credits} {credits}");
+
+    Console.WriteLine(displayFormat, 0, "Quitter");
+}
+
+void ExecuteActionByChoice(int choice)
+{
+    const int NewGame = 1;
+    const int LoadGame = 2;
+
+    switch (choice)
+    {
+        case NewGame:
+            {
+                Console.WriteLine("On démarre une partie");
+            }
+            break;
+
+        case LoadGame:
+            {
+                Console.WriteLine("Quelle partie à charger");
+                //string[][][][] tableauDeTableau;
+                //string[,,,] matrice;
+
+                //string[] datas = new string[2];
+                //datas[0] = "Data 1";
+
+
+
+                DateTime currentDate = DateTime.Now.AddDays(-1).AddMinutes(30);
+                DateOnly dateOnly;
+                TimeOnly timeOnly;
+
+                string[] datas = [string.Format("Data {0:dd/MM/yyyy}", currentDate), $"Data {DateTime.Now.ToString("dddd dd MMMM yy HH:mm:ss")}", $"Data {DateTime.Now.ToString()}"]; // dotnet 8
+                for (int i = 0; i < datas.Length; i++)
+                {
+                    Console.WriteLine(datas[i]);
+                }
+            }
+            break;
+
+        default:
+            break;
+    }
+}
+
+int GetLastIndex()
+{
+    return 3;
+}
+#endregion
+
+
+Console.ReadLine();
---
+++ FirstBisProject/FirstBisProject/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+
+string title = "a sam journey";
+var subTitle = "A orsys game";
+
+char oneChar = 'A';
+var oneChar2 = "A";
+
+// Console.WriteLine(title.ToUpper().ToLower());
+Console.WriteLine(title.Substring(0, 1).ToUpper() + title.Substring(1));
+Console.WriteLine(subTitle[0]);
+
+DisplayMenu();
+
+#region Learnings
+void ConcatListOfStrings()
+{
+    string content = string.Empty;
+    StringBuilder builder = new StringBuilder();
+
+    for (int i = 0; i < 1100; i++)
+    {
+        //content += "i : " + i.ToString();
+        // content += string.Format("i : {0}", i);
+        // content += $"i : {i}";
+        builder.Append($"i : {i}");
+    }
+
+    content = builder.ToString();
+}
+
+void CastValue()
+{
+    #region number
+    int iOne = 1;
+    decimal dTwo = 2.0M;
+    float fThree = 3.0F;
+    double ddFour = 4.0D;
+    long lFive = 5L;
+
+    dTwo = iOne; // conversion implicite, car type inférieur
+    iOne = (int)dTwo; // brut cast / force cast : attention on perd des données binaires ! 
+
+    // int.MinValue
+    #endregion
+
+    #region Cast string / number
+    int resultInt = int.Parse("1");
+    resultInt = Convert.ToInt32("1");
+    bool boolResult = bool.Parse("true");
+    #endregion
+}
+#endregion
+
+#region Coeur du jeu vidéo
+void DisplayMenu()
+{
+    string displayFormat = "{0}. {1}";
+
+    Console.WriteLine(string.Format(displayFormat, 1.ToString(), "Nouvelle partie", "toto"));
+    Console.WriteLine(string.Format(displayFormat, 2, "Charger une partie"));
+
+    //Console.WriteLine(string.Format(displayFormat, 3, "Credits"));
+    var credits = "Crédits";
+    Console.WriteLine($"{GetLastIndex()}. {credits} {credits}");
+
+    Console.WriteLine(displayFormat, 0, "Quitter");
+}
+
+int GetLastIndex()
+{
+    return 3;
+}
+#endregion
