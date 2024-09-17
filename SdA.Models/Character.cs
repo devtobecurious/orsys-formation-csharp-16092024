16ab8600f1917164768283e3b3b6658ca6687a56
Evan BOISSONNOT <evan@boissonnot.fr>
2024-09-17 17:01:27 +0200

    feat: fin de seconde journée

---
+++ SdA.Models/Character.cs
@@ -0,0 +1,19 @@
+﻿namespace SdA.Models
+{
+    internal class Character
+    {
+        #region Constructors
+        public Character(string surname, int strength)
+        {
+            this.Surname = surname;
+            this.Strength = strength;
+        }
+        #endregion
+
+        #region Properties
+        public required string Surname { get; set; }
+
+        public int Strength { get; init; }
+        #endregion
+    }
+}
